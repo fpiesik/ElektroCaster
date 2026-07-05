7268eba5a2325b09c1c39b651e1a8f7b71ef5036
Frank Piesik <f.piesik@gmail.com>
2026-07-05 12:33:17 +0200

    Centralize firmware hardware pin arrays

---
+++ software/firmwareHid/HardwareConfig.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <Arduino.h>
+
+const int nAnalog = 22;
+const int nDigital = 19;
+const int nEnc = 8;
+
+const int aPin[nAnalog] = {31, 32, 33, 34, 35, 36, 37, 38, 39, A21, A22, 14, A26, A25, A11, A10, A24, A23, A9, A8, A7, A6 };
+const int dPin[nDigital] = {17, 13, 51, 52, 53, 42, 41, 40, 56, 57, 43, 54, 55, 4, 7, 10, 25, 27, 30};
