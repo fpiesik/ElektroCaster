3e3f43613b7b76c381ca5bbcaa2736005463fd03
Frank Piesik <f.piesik@gmail.com>
2026-09-19 16:36:42 +0200

    Update 06_midi.ino

--- software/firmwareCtl/06_midi.ino
+++ software/firmwareCtl/06_midi.ino
@@ -34,7 +34,7 @@ void sndMidiNotePress(int str, int frt, int chn){
 //    }
     int note=tuning[str]+frt+12;
     sndMidiNote(note,127, chn);
-    delay(10);
+    delay(1);
     if (lastNote[str]!=0) sndMidiNote(lastNote[str],0, lastChn[str]);
     lastNote[str]=note;
     lastChn[str]=chn;
